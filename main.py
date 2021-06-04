29f2def06a621ea6d6dd7ae0fdfbd4ea61f80a49
muyun <wenlzhao@gmail.com>
2021-06-04 17:55:48 +0800

    add

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
+#
+import cmarkgfm
+import frontmatter
+import pathlib
+from typing import Iterator
+
+
+def render_markdown(markdown_text: str) -> str:
+    content = cmarkgfm.markdown_to_html_with_extensions(
+        markdown_text, extensions=["table", "autolink", "strikethrough"]
+    )
+
+    return content
+
+
+# check more pathlib
+def get_sources() -> Iterator[pathlib.Path]:
+    return pathlib.Path(".").glob("pages/*.md")
+
+
+def parse_source(source: pathlib.Path) -> frontmatter.Post:
+    post = frontmatter.load(str(source))
+    return post
+
+
 def main():
+    doc = "pages/hello.md"
+    print(render_markdown(doc))
     return
 
 
---
+++ pages/hello.md
@@ -0,0 +1,6 @@
+# hello.html
+title: Hello
+date: 2021-03-13
+tags: test, hello
+
+Hello, World, I am coming ... 
