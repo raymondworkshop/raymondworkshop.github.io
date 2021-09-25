1736f7372227211f5c0eb20d2250b3d189a9e956
muyun <wenlzhao@gmail.com>
2021-09-25 19:06:50 +0800

    update art

--- _posts/art.md
+++ _posts/art.md
@@ -3,8 +3,7 @@ title: Art
 date: 2021-08-05
 ---
 
-#### HK in the 1970s
-* ![03](/img/03.jpg)  
-* ![04](/img/04.jpg)  
-* ![01](/img/01.jpg)  
-* ![02](/img/02.jpg) 
+#### Historical photographs
+* ![03](/img/03.jpg "HongKong in the 1970s")   
+* ![04](/img/04.jpg "HongKong in the 1970s")   
+* ![05](/img/05.jpg "Shanghai in the 1980s") 
--- img.py
+++ img.py
@@ -1,9 +1,10 @@
 from PIL import Image
 
-img = Image.open('/Users/zhaowenlong/workspace/myblog/docs/img/02.jpg')
+img = Image.open('/Users/zhaowenlong/workspace/myblog/docs/img/_05.jpg')
 img.show()
+print(img.size)
 
-resized_img = img.resize((round(img.size[0]*0.5), round(img.size[1]*0.5)))
+resized_img = img.resize((500, round(img.size[1]*0.5)))
 resized_img.show()
 
-resized_img.save('/Users/zhaowenlong/workspace/myblog/docs/img/_02.jpg')
+resized_img.save('/Users/zhaowenlong/workspace/myblog/docs/img/05.jpg')
