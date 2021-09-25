9d978fcf08b2a63f2394184989defa799cd0035e
muyun <wenlzhao@gmail.com>
2021-09-25 14:23:28 +0800

    update

--- serve.py
+++ serve.py
@@ -11,6 +11,6 @@ def rebuild():
 server = livereload.Server()
 server.setHeader("Cache-Control", "no-store")
 server.watch("_posts/*.md", rebuild)
-#server.watch("docs/**")
+server.watch("docs/**")
 server.watch("templates/**.html", rebuild)
 server.serve(root="docs", port=4000)
