76de5db4912f43d22b9869b5383c2d891141617f
muyun <wenlzhao@gmail.com>
2022-06-20 13:08:49 +0800

    update

--- style.py
+++ style.py
@@ -15,87 +15,87 @@ class themeStyle(Style):
     This style is a witchy theme 
     """
 
-    background_color = "#433e56"
-    highlight_color = "#716799"
+    background_color = "#f8f8f8"
+    highlight_color = "#ffffcc"
 
     styles = {
         # No corresponding class for the following:
-        Text:                      "#F8F8F2",  # class:  ''
-        Whitespace:                "#A8757B",        # class: 'w'
-        Error:                     "#960050 bg:#1e0010",  # class: 'err'
+        Text:                      "",  # class:  ''
+        Whitespace:                "#bbbbbb",        # class: 'w'
+        Error:                     "#FF0000",  # class: 'err'
         Other:                     "",        # class 'x'
 
-        Comment:                   "#b0bec5",  # class: 'c'
-        Comment.Multiline:         "",        # class: 'cm'
-        Comment.Preproc:           "",        # class: 'cp'
-        Comment.Single:            "",        # class: 'c1'
-        Comment.Special:           "",        # class: 'cs'
+        Comment:                   "#008800",  # class: 'c'
+        Comment.Multiline:         "#008800",        # class: 'cm'
+        Comment.Preproc:           "#008800",        # class: 'cp'
+        Comment.Single:            "#008800",        # class: 'c1'
+        Comment.Special:           "#008800",        # class: 'cs'
 
-        Keyword:                   "#C2FFDF",  # class: 'k' italic?
+        Keyword:                   "#aa22FF",  # class: 'k' italic?
         Keyword.Constant:          "",        # class: 'kc'
         Keyword.Declaration:       "",        # class: 'kd' italic?
-        Keyword.Namespace:         "#FFB8D1",  # class: 'kn'
+        Keyword.Namespace:         "#aa22FF",  # class: 'kn'
         Keyword.Pseudo:            "",        # class: 'kp'
         Keyword.Reserved:          "",        # class: 'kr'
         Keyword.Type:              "",        # class: 'kt' italic?
 
-        Operator:                  "#FFB8D1",  # class: 'o'
-        Operator.Word:             "",        # class: 'ow' - like keywords
+        Operator:                  "#666666",  # class: 'o'
+        Operator.Word:             "#AA22FF",        # class: 'ow' - like keywords
 
-        Punctuation:               "#F8F8F2",  # class: 'p'
+        Punctuation:               "",  # class: 'p'
 
-        Name:                      "#F8F8F2",  # class: 'n'
-        Name.Attribute:            LILAC,  # class: 'na'
-        Name.Builtin:              "",        # class: 'nb'
-        Name.Builtin.Pseudo:       "#80cbc4",        # class: 'bp'
-        Name.Class:                LILAC,  # class: 'nc' italic underline?
-        Name.Constant:             "#C5A3FF",  # class: 'no'
-        Name.Decorator:            LILAC,  # class: 'nd' underline?
-        Name.Entity:               "",        # class: 'ni'
-        Name.Exception:            LILAC,  # class: 'ne' underline?
-        Name.Function:             LILAC,  # class: 'nf'
-        Name.Property:             "#F8F8F2",        # class: 'py'
-        Name.Label:                "",        # class: 'nl'
-        Name.Namespace:            "",        # class: 'nn' - to be revised
+        Name:                      "#B8860B",  # class: 'n'
+        Name.Attribute:            "#BB4444",  # class: 'na'
+        Name.Builtin:              "#AA22FF",        # class: 'nb'
+        Name.Builtin.Pseudo:       "#AA22F",        # class: 'bp'
+        Name.Class:                "#0000FF",  # class: 'nc' italic underline?
+        Name.Constant:             "#880000",  # class: 'no'
+        Name.Decorator:            "#AA22FF",  # class: 'nd' underline?
+        Name.Entity:               "#999999",        # class: 'ni'
+        Name.Exception:            "#D2413A",  # class: 'ne' underline?
+        Name.Function:             "#00A000",  # class: 'nf'
+        Name.Property:             "",        # class: 'py'
+        Name.Label:                "#A0A000",        # class: 'nl'
+        Name.Namespace:            "#0000FF",        # class: 'nn' - to be revised
         Name.Other:                "",  # class: 'nx'
-        Name.Tag:                  "#FFB8D1",  # class: 'nt' - like a keyword
-        Name.Variable:             "#F8F8F2",        # class: 'nv' - to be revised
-        Name.Variable.Class:       "",        # class: 'vc' - to be revised
-        Name.Variable.Global:      "",        # class: 'vg' - to be revised
-        Name.Variable.Instance:    "",        # class: 'vi' - to be revised
+        Name.Tag:                  "#008000",  # class: 'nt' - like a keyword
+        Name.Variable:             "#B8860B",        # class: 'nv' - to be revised
+        Name.Variable.Class:       "#B8860B",        # class: 'vc' - to be revised
+        Name.Variable.Global:      "#B8860B",        # class: 'vg' - to be revised
+        Name.Variable.Instance:    "#B8860B",        # class: 'vi' - to be revised
 
-        Number:                    "#C5A3FF",  # class: 'm'
-        Number.Float:              "",        # class: 'mf'
-        Number.Hex:                "",        # class: 'mh'
-        Number.Integer:            "",        # class: 'mi'
-        Number.Integer.Long:       "",        # class: 'il'
-        Number.Oct:                "",        # class: 'mo'
+        Number:                    "#666666",  # class: 'm'
+        Number.Float:              "#666666",        # class: 'mf'
+        Number.Hex:                "#666666",        # class: 'mh'
+        Number.Integer:            "#666666",        # class: 'mi'
+        Number.Integer.Long:       "#666666",        # class: 'il'
+        Number.Oct:                "#666666",        # class: 'mo'
 
-        Literal:                   "#ae81ff",  # class: 'l'
-        Literal.Date:              "#e6db74",  # class: 'ld'
+        Literal:                   "#BB4444",  # class: 'l'
+        Literal.Date:              "#BB4444",  # class: 'ld'
 
-        String:                    TORQUOISE,  # class: 's'
-        String.Backtick:           "",        # class: 'sb'
-        String.Char:               "",        # class: 'sc'
-        String.Doc:                "",        # class: 'sd' - like a comment
-        String.Double:             "",        # class: 's2'
-        String.Escape:             "",  # class: 'se'
-        String.Heredoc:            "",        # class: 'sh'
-        String.Interpol:           "",        # class: 'si'
-        String.Other:              "",        # class: 'sx'
-        String.Regex:              "",        # class: 'sr'
-        String.Single:             "",        # class: 's1'
-        String.Symbol:             "",        # class: 'ss'
+        String:                    "#BB4444",  # class: 's'
+        String.Backtick:           "#BB4444",        # class: 'sb'
+        String.Char:               "#BB4444",        # class: 'sc'
+        String.Doc:                "BB4444",        # class: 'sd' - like a comment
+        String.Double:             "BB4444",        # class: 's2'
+        String.Escape:             "#BB6622",  # class: 'se'
+        String.Heredoc:            "#BB4444",        # class: 'sh'
+        String.Interpol:           "#BB6688",        # class: 'si'
+        String.Other:              "#008000",        # class: 'sx'
+        String.Regex:              "#BB6688",        # class: 'sr'
+        String.Single:             "#BB4444",        # class: 's1'
+        String.Symbol:             "#B8860B",        # class: 'ss'
 
         Generic:                   "",        # class: 'g'
-        Generic.Deleted:           "#f92672",  # class: 'gd',
+        Generic.Deleted:           "#A00000",  # class: 'gd',
         Generic.Emph:              "italic",  # class: 'ge'
-        Generic.Error:             "",        # class: 'gr'
-        Generic.Heading:           "",        # class: 'gh'
-        Generic.Inserted:          "#a6e22e",  # class: 'gi'
-        Generic.Output:            "",        # class: 'go'
-        Generic.Prompt:            "",        # class: 'gp'
+        Generic.Error:             "#FF0000",        # class: 'gr'
+        Generic.Heading:           "#000080",        # class: 'gh'
+        Generic.Inserted:          "#00A000",  # class: 'gi'
+        Generic.Output:            "#888888",        # class: 'go'
+        Generic.Prompt:            "#000080",        # class: 'gp'
         Generic.Strong:            "bold",    # class: 'gs'
-        Generic.Subheading:        "#75715e",  # class: 'gu'
-        Generic.Traceback:         "",        # class: 'gt'
+        Generic.Subheading:        "#800080",  # class: 'gu'
+        Generic.Traceback:         "#0044DD",        # class: 'gt'
     }
