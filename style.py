21e426b474ff53065a8b5428263a57ef3b6a6e94
muyun <wenlzhao@gmail.com>
2022-07-14 14:29:11 +0800

    update

--- style.py
+++ style.py
@@ -15,7 +15,7 @@ class themeStyle(Style):
     This style is a vs theme 
     """
 
-    background_color = "#f6f6f6"
+    background_color = "#f7f7f8"
    # highlight_color = "#ffffcc"
 
     styles = {
